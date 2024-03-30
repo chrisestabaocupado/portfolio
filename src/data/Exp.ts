4d7fd981e9883b629a6cbc2bdf3b5ec728ef0f46
Christopher Glood <christopherglood@gmail.com>
2024-03-29 20:23:15 -0400

    creating mocks for testing

---
+++ src/data/Exp.ts
@@ -0,0 +1,10 @@
+export const expMock = [
+  {
+    url: "https://fasmee.gob.ve/",
+    date: "Septiembre 2021 - Marzo 2024",
+    title: "Fondo Autoadministrado de Salud del MPPEE",
+    details: "Programador de Sistemas",
+    description:
+      "Desarrollo, puesta en producción y mantenimiento de Sitio Web Institucional, configuración de servicios virtualizados en PROXMOX y Soporte Técnico.",
+  },
+];
---
+++ src/data/Projects.ts
@@ -0,0 +1,8 @@
+export const projectMock = [
+  {
+    title: "Python",
+    description: "Nociones basicas de Python",
+    url: "https://github.com/chrisestaba/Python",
+    tags: ["Python", "Programacion"],
+  },
+];
