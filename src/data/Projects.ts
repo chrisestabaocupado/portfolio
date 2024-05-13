27d3fb0e869436a73f94c650343c1f9760ac9099
Christopher Glood <christopherglood@gmail.com>
2024-05-12 23:23:31 -0400

    guardando datos localmente y remplazando api

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -1,39 +1,17 @@
 export interface Project {
-  name: string;
-  language: string;
+  title: string;
+  tags: string[];
   description: string;
-  html_url: string;
-  topics: string[];
+  project_url: string;
+  img_url: string;
 }
 
-export const GetProyectsPagination = async (
-  token: string,
-  page: number,
-  items: number
-) => {
-  const GetProjects = fetch(
-    `https://api.github.com/user/repos?page=${page}&per_page=${items}`,
-    {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    }
-  )
-    .catch(err => console.error(err))
-    .then((res) => res.json())
-    .then((data) => {
-      return data.map((project: Project) => {
-        return {
-          name: project.name,
-          language: project.language,
-          description: project.description,
-          html_url: project.html_url,
-          topics: project.topics.filter(
-            (tag: string) => project.topics.indexOf(tag) < 2
-          ),
-        };
-      });
-    });
-  const Projects: Project[] = await GetProjects;
-  return Projects;
-};
+export const Projects: Project[] = [
+	{
+		title: "FASMEE",
+		tags: ["Javascript"],
+		description: "Sitio web institucional para una aseguradora",
+		project_url: "https://fasmee.gob.ve/",
+		img_url: "/nvim.png"
+	}
+];
