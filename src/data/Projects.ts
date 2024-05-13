fea40e94c703a137974049a9013d276a080178b3
Christopher Glood <christopherglood@gmail.com>
2024-05-13 01:08:58 -0400

    mejorando cards de proyectos falta event listener

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -1,15 +1,15 @@
-export interface Project {
-  title: string;
-  tags: string[];
-  description: string;
-  project_url: string;
-  img_url: string;
+interface Project {
+	title: string;
+	tags: string[];
+	description: string;
+	project_url: string;
+	img_url: string;
 }
 
 export const Projects: Project[] = [
 	{
 		title: "FASMEE",
-		tags: ["Javascript"],
+		tags: ["Javascript", "PUG", "ExpressJS", "Bootstrap"],
 		description: "Sitio web institucional para una aseguradora",
 		project_url: "https://fasmee.gob.ve/",
 		img_url: "/nvim.png"
