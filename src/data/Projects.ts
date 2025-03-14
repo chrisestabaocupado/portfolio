361c9b571e85280920eb8ae497624c8a75901c25
Christopher Glood <christopherglood@gmail.com>
2025-03-14 15:48:26 -0400

    minor fixes

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -7,18 +7,19 @@ interface Project {
 }
 
 export const Projects: Project[] = [
-	{
-		title: "FASMEE",
-		tags: ["Javascript", "PUG", "ExpressJS", "Bootstrap"],
-		description: "Sitio web institucional para una aseguradora",
-		project_url: "https://fasmee.gob.ve/",
-		img_url: "/projects/fasmee.png"
-	},
-	{
-		title: "ecommerce",
-		tags: ["ExpressJS", "Bootstrap", "PUG"],
-		description: "Sitio web de comercio electrónico",
-		project_url: "https://github.com/chrisestabaocupado/ecommerce",
-		img_url: "/projects/nvim.png"
-	}
+  {
+    title: "FASMEE",
+    tags: ["Javascript", "PUG", "ExpressJS", "Bootstrap"],
+    description:
+      "El sitio web institucional del FASMEE es una plataforma moderna y accesible que informa a los usuarios sobre los servicios, logros y actividades de la institución, facilitando el acceso a contenido relevante de manera clara y organizada.",
+    project_url: "https://fasmee.gob.ve/",
+    img_url: "/projects/fasmee.png",
+  },
+  {
+    title: "ecommerce",
+    tags: ["ExpressJS", "Bootstrap", "PUG"],
+    description: "Sitio web de comercio electrónico",
+    project_url: "https://github.com/chrisestabaocupado/ecommerce",
+    img_url: "/projects/nvim.png",
+  },
 ];
