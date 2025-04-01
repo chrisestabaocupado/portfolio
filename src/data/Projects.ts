6b0595fc5ec13542e2eb569b8df6aaf1c0d6ca46
Christopher Glood <christopherglood@gmail.com>
2025-04-01 15:02:51 -0400

    animations test

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -15,7 +15,7 @@ export const Projects: Project[] = [
     description:
       "Sitio web institucional del FASMEE con información sobre servicios y actividades, diseñado para ser accesible y fácil de navegar.",
     project_url: "https://fasmee.gob.ve/",
-    img_url: "/projects/fasmee.png",
+    img_url: "/projects/fasmee.webp",
   },
   {
     id: "python",
@@ -23,7 +23,7 @@ export const Projects: Project[] = [
     tags: ["Python"],
     description:
       "Repositorio con fundamentos de Python, ideal para aprender y reforzar conceptos básicos y avanzados.",
-    img_url: "/projects/python.png",
+    img_url: "/projects/python.webp",
     project_url: "https://github.com/chrisestabaocupado/python",
   },
 ];
