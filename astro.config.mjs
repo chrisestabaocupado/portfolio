bc679b5c5042666c7bf24305a4880a93b1d53229
Christopher Glood <christopherglood@gmail.com>
2026-01-18 14:08:34 -0400

    major changes: color palette, timeline and layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [tailwind()]
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
